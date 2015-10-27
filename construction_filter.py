64b2eb29068bde1c1bcaf56aa7a81c590f8060d9
Canh Tran <onimusha0611@yahoo.com>
2015-10-27 19:22:15 +0800

    Code filter

--- construction_filter.py
+++ construction_filter.py
@@ -7,13 +7,14 @@ for index, row in dummy_data.iterrows():
 	if row['industry'] == "ERROR":
 		print index
 	elif int(row['industry']) in range(1500,1800):
-		list_id.append(row['id']) 
+		list_id.append(row['id'])
 
 osha_data = pd.read_excel("data/osha.xlsx")
 osha_data.columns = ['id', 'title', 'summary']
 
 temp = pd.DataFrame()
+l = []
 for i in list_id:
-	temp = osha_data[osha_data['id'] == i]
-
-print temp
+	l.append(osha_data[osha_data['id'] == i])
+temp = pd.concat(l)
+temp.to_csv("osha_filter.csv")
